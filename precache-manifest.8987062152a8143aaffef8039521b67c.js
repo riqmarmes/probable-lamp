95aed78d8fb54dfdba345da1c9b028c2882da0f0
riqmarmes <theomar.itani@gmail.com>
2020-05-15 23:58:04 +0300

    Updates

---
+++ precache-manifest.8987062152a8143aaffef8039521b67c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e291ea8251b3f1b84f59da556c70be",
+    "url": "/probable-lamp/index.html"
+  },
+  {
+    "revision": "ea6749c5a00af94acb2c",
+    "url": "/probable-lamp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96c842ffd0c7dcf73d0b",
+    "url": "/probable-lamp/static/js/2.225f57ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/probable-lamp/static/js/2.225f57ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6749c5a00af94acb2c",
+    "url": "/probable-lamp/static/js/main.185e1682.chunk.js"
+  },
+  {
+    "revision": "162e634c7bbe12017d3e",
+    "url": "/probable-lamp/static/js/runtime-main.f8dd1a1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/probable-lamp/static/media/logo.5d5d9eef.svg"
+  }
+]);
